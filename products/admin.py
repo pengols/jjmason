6af7e7dec30a86fd38c9cadf90a88c60f5d6ee6f
pengols <pengelly30@hotmail.com>
2020-12-01 18:43:36 +0000

    added additional info to admin panel vor categories and products

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'isbn',
+        'category',
+        'name',
+        'original_author',
+        'illustrator',
+        'price',
+        'publication_year',
+        'publisher',
+    )
+
+    ordering = ('isbn',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=128)
     friendly_name = models.CharField(max_length=128, blank=True, null=True)
 
@@ -24,7 +28,7 @@ class Product(models.Model):
     language = models.CharField(max_length=254, null=True, blank=True)
     num_pages = models.DecimalField(max_digits=4, decimal_places=0,null=True, blank=True)
     publication_year = models.DecimalField(max_digits=4, decimal_places=0, null=True, blank=True)
-    Publisher = models.CharField(max_length=254, null=True, blank=True)
+    publisher = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
