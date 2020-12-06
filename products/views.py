0609335a6846d341cf8ca4a46b377c34c76aec58
pengols <pengelly30@hotmail.com>
2020-12-06 12:37:10 +0000

    added categories dropdown in nav and filtering for products

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.db.models import Q
 from django.contrib import messages
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -11,8 +11,10 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -22,9 +24,16 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(publisher__icontains=query) | Q(illustrator__icontains=query) | Q(isbn__icontains=query) | Q(original_author__icontains=query)
             products = products.filter(queries)
 
+        if request.GET:
+            if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                products = products.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
+
     context = {
         'products': products,
         'search': query,
+        'categories': categories,
     }
 
     return render(request, 'products/products.html', context)
