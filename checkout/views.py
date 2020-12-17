a6f2164942f577c3b31d7b3d87d3c37f738543cb
pengols <pengelly30@hotmail.com>
2020-12-17 21:00:53 +0000

    added stripe js and html

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HzGWeL0FWth1zIODW79XPQuZI8WfQVHIzCLItbUhgZhpOZqkV6RxEJiJbD8bRugcZH2080HYa87UsmdcKcsRuj000IUDc5XN3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
