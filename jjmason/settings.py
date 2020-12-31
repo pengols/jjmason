41ed090d51a3ba021c016db18330114ce06b41a9
pengols <pengelly30@hotmail.com>
2020-12-31 09:36:23 +0000

    updated and removed secret key from settings.py and set debug to development only

--- jjmason/settings.py
+++ jjmason/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fvz3w%8&tv)61loibimt)zp!&1ob5t7kr5n$@ba!k!#f64$@-g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['jjmason.herokuapp.com', 'localhost']
 
