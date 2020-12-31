4768e5707f5bd363f9be10d87d3d3d6c720bf64e
pengols <pengelly30@hotmail.com>
2020-12-31 09:25:08 +0000

    disabled staticfile heroku and added app to allowed hosts in settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jjmason.wsgi:application
--- jjmason/settings.py
+++ jjmason/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'fvz3w%8&tv)61loibimt)zp!&1ob5t7kr5n$@ba!k!#f64$@-g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jjmason.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'jjmason.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
