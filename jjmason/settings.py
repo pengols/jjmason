4b7c1251caed9af8c108e642139b0c1ac0d28d34
pengols <pengelly30@hotmail.com>
2021-01-26 19:46:03 +0000

    temp add of x_frame_options = http://ami.responsivedesign.is/ to get screen shot of site for readme

--- jjmason/settings.py
+++ jjmason/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+X_FRAME_OPTIONS = 'ALLOW-FROM http://ami.responsivedesign.is/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
