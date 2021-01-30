40770777f17263eb9da81f55e8fd6cac59df65ec
pengols <pengelly30@hotmail.com>
2021-01-30 10:54:15 +0000

    added subtotal functionality for shopping cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -7,9 +7,7 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
-    # delivery = 4.95
     cart = request.session.get('cart', {})
-    # grand_total = delivery + total
 
     for item_id, quantity in cart.items():
         if isinstance(quantity, int):
@@ -37,8 +35,6 @@ def cart_contents(request):
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
-        # 'delivery': delivery,
-        # 'grand_total': grand_total,
     }
 
     return context
